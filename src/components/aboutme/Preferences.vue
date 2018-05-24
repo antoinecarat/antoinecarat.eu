7dee25d674827840bf22cbcfcb0940e4e410e1e0
cara-puce <antoine.carat@gmail.com>
2018-05-24 08:46:03 +0200

    Start preferences

---
+++ src/components/aboutme/Preferences.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p class="subtitle has-text-centered">{{title}}</p>
+    <div class="" v-for="comp in comparisons">
+      <i :class="comp['icon-left']"></i><i :class="comp['icon-right']"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+import datas from './../../data/en/preferences.js'
+
+export default {
+  props: ['title'],
+  data: function () {
+    return datas
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/data/en/preferences.js
@@ -0,0 +1,10 @@
+export default {
+  comparisons: [
+    {
+      "icon-left": "fas fa-user",
+      "icon-right": "fas fa-star",
+      value: 50,
+      description: "Left or right, can't decide"
+    }
+  ]
+}
