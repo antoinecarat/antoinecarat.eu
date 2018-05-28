29f4a7abf48398c0793a93876d5cf9219ab1ac52
cara-puce <antoine.carat@gmail.com>
2018-05-28 09:40:34 +0200

    Divide Career into components

--- src/components/Career.vue
+++ src/components/Career.vue
@@ -1,31 +1,21 @@
 <template>
   <div id="career" class="columns">
     <div id="experiences" class="timeline column is-one-quarter">
-      <div class="timeline-item" v-for="(experience, index) in experiences">
-        <div class="timeline-marker is-image is-48x48">
-          <img :src="experience.logo">
-        </div>
-        <div class="timeline-content">
-          <p class="heading"><span class="tag is-success" v-show="experience.current">Current</span> {{experience.title}}</p>
-          <small class="is-italic"><i class="fas fa-map-marker-alt"></i> {{experience.location}}</small> <br/>
-          <small class="is-italic"><i class="far fa-calendar"></i> {{experience.dates}}</small> <br/>
-          <small class="is-italic tags"><i class="fas fa-tag"></i> &ensp; <span class="tag" v-for="tag in experience.tags">{{tag}}</span></small>
-        </div>
-      </div>
+      <careeritem :icon="experience.logo" :title="experience.title" :dates="experience.dates" :location="experience.location" :tags="experience.tags" :description="experience.description" :current="experience.current" v-for="experience in experiences" :keys="experience.title">
+      </careeritem>
       <div class="timeline-header">
         <span class="tag is-medium is-link">2012</span>
       </div>
-    </div>
-    <div class="column is-three-quarter is-hidden-touch">
-      <p v-for="(experience, index) in experiences">{{experience.description}}</p>
     </div>
   </div>
 </template>
 
 <script>
 import careerDatas from '../data/en/career.js'
+import CareerItem from './career/CareerItem.vue'
 
 export default {
+  components: { careeritem: CareerItem },
   data () {
     return careerDatas
   }
---
+++ src/components/career/CareerItem.vue
@@ -0,0 +1,47 @@
+<template>
+<div>
+  <div class="timeline-item"  @click="showDescription=!showDescription" :class="{'is-info': showDescription}">
+      <div class="timeline-marker is-image is-48x48">
+        <img :src="logo">
+      </div>
+      <div class="timeline-content">
+        <p class="heading"><span class="tag is-success" v-show="current">Current</span> {{title}}</p>
+        <small class="is-italic"><i class="fas fa-map-marker-alt"></i> {{location}}</small> <br/>
+        <small class="is-italic"><i class="far fa-calendar"></i> {{dates}}</small> <br/>
+        <small class="is-italic tags"><i class="fas fa-tag"></i> &ensp; <span class="tag" v-for="tag in tags">{{tag}}</span></small>
+        <p>
+          <span v-if="!showDescription"><a>More...</a></span>
+          <span v-else><a>Fold</a></span>
+        </p>
+      </div>
+  </div>
+  <div class="modal" :class="{'is-active': showDescription }">
+    <div class="modal-background" @click="showDescription = !showDescription"></div>
+    <div class="modal-content">
+      <div class="card is-8">
+        <p>
+          {{description}}
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+
+export default {
+  props: ['logo', 'title', 'dates', 'location', 'tags', 'description', 'current'],
+  data: function () {
+    return {
+      showDescription: false
+    }
+  }
+}
+</script>
+
+<style>
+  .timeline-item {
+    cursor: pointer;
+  }
+</style>
