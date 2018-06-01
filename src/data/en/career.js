baa1449c340b8aaa8b8ad03cec318c0874186297
cara-puce <antoine.carat@gmail.com>
2018-06-01 09:41:53 +0200

    Edit career datas

--- src/data/en/career.js
+++ src/data/en/career.js
@@ -1,65 +1,75 @@
 export default {
   experiences: [
     {
-      title: 'Master degree internship',
-      location: 'VSCT, Nantes, France',
-      dates: '15/01/2018 - 13/07/2018',
+      title: 'DevOps Intern',
+      location: 'VSC-Technologies, Nantes, France',
+      dates: '18/04/2018 - 13/07/2018',
       description:
-        '2 years before this intership I\'ve discovered Cloud computing world and DevOps fashion and this was my chance to jump right in.\
-         Working with technologies like OpenStack, Jenkins, Docker, ... taught me how things work in the Ops side of DevOps, which will for sure reveal itself as a strengh.\
-         I also built up a solution testing an application resiliency with chaos. I was the monkey, i definitely loved it! :)',
+        'Chaos tests automation',
       logo: 'https://bulma.io/images/placeholders/128x128.png',
-      tags: ['OpenStack', 'Docker', 'Docker-Swarm', 'Jenkins', 'OpenFaas', 'Angular', 'Go', 'AWS', 'Chaos engineering'],
+      tags: ['Chaos engineering', 'Python', 'Jenkins', 'AWS'],
       current: true
     },
     {
-      title: 'Volunteer internship',
+      title: 'Cloud Intern',
+      location: 'VSC-Technologies, Nantes, France',
+      dates: '15/01/2018 - 18/04/2018',
+      description:
+        'OpenStack prod platform management and customer support',
+      logo: 'https://bulma.io/images/placeholders/128x128.png',
+      tags: ['OpenStack', 'Docker', 'Docker-Swarm', 'Jenkins', 'OpenFaas', 'Angular', 'Go']
+    },
+    {
+      title: 'Cloud Research Intern',
       location: 'Discovery initiative, IMT, Nantes, France',
       dates: '15/06/2017 - 15/08/2017',
       description:
-        'During this internship I continue to work on the highly-distributed OpenStack project lead by Discovery initiative. \
-         This time, i was in charge of switching one OpenStack service backend from a relationnal database to a distributed one.',
+        'During this volunteer internship I continue to work on the highly-distributed OpenStack project led by Discovery initiative. \
+         This time, I was in charge of moving one OpenStack service backend from a relationnal database (mySQL) to a distributed one (CockroachDB).',
       logo: 'https://bulma.io/images/placeholders/128x128.png',
-      tags: ['OpenStack', 'CockroachDB', 'Ansible', 'Python', 'Research']
+      tags: ['OpenStack', 'Ansible', 'Python', 'CockroachDB', 'Research']
     },
     {
-      title: 'Research initiation internship',
+      title: 'Research initiation Internship',
       location: 'Discovery initiative, IMT, Nantes, France',
       dates: '15/01/2017 - 15/04/2017',
       description:
-        'Once a week, we worked with the Discovery initiative team around their highly-distributed OpenStack project. \
-         Our work was to add a code profiling tool to Enos, a tool developped by the team.',
+        'Once a week, we worked with the Discovery initiative team around their highly-distributed OpenStack project in order to discover the research world. \
+         Our work was to add a code profiling tool to Enos, a tool developped by the team that allows to launch experiments on OpenStack.',
       logo: 'https://bulma.io/images/placeholders/128x128.png',
       tags: ['OpenStack', 'Ansible', 'Python', 'Research']
     },
     {
       title:
-        'Non-profit student organization fundation & presidency : Alice&Bob',
+        'Non-profit student organisation founding president: Alice&Bob',
       location: 'Sciences University, Nantes, France',
       dates: '25/10/2016 - 30/10/2017',
       description:
-        'I founded and take care of a non-profit organization in my University. the goal of this organization is to build up a "school-spirit" between all nantes\' \
-         University IT students with parties but also helping them in their shoolarship by organizing events like professionnal meetings for example.',
+        'I founded and take care of a non-profit organisation in my University. The goal of Alice&Bob is to build up a "school-spirit" between all nantes\' \
+         University IT students by helping them in their shoolarship by organizing events like professionnal meetings, school program explainations, ... \
+         The organisation also takes care of the advertisement of students by organizing parties or cohesion events.',
       logo: 'https://bulma.io/images/placeholders/128x128.png',
       tags: ['Team management', 'Event organization', 'Self-control']
     },
     {
-      title: 'Bachelor degree internship',
+      title: 'Constraint Programming Intern',
       location: 'TASC team, EMN, Nantes, France',
       dates: '18/04/2016 - 08/07/2016',
       description:
-        'During this internship, i was in charge of enhancing a Constraint Problem solver named Choco by externalizing some particular problems \
-         to a specif solver.',
+        'I ended my Bachelor degree as a two-months intern in the research team TASC, standing for Theorie, Algorithms and Constraints Systems. \
+        During this internship, I was in charge of enhancing a Constraint Problem solver named Choco by externalizing all linear constraints to a dedicated solver: IntSat.',
       logo: 'https://bulma.io/images/placeholders/128x128.png',
-      tags: ['Constraint Programming', 'Java', 'Research']
+      tags: ['Constraint Programming', 'Java', 'Software Architecture', 'Research']
     },
     {
-      title: 'DUT internship',
+      title: 'Automation and Deployment Intern',
       location: 'La Poste DTC, Nantes, France',
       dates: '18/04/2014 - 30/06/2014',
       description:
-        'Mail-sorting engines and servers deployements are automated with Puppet. The probleme was that these automation scripts were written in a old version of this language. \
-         So my job was to migrate these scripts in a much recent version of Puppet including good practices etc.',
+        'The last two month of DUT are dedicated to an internship. I made mine in the Technical Direction of the french postal service, La Poste. \
+        Mail-sorting engines and servers deployements are automated with Puppet, a automation language written in Ruby. \
+        The problem here was that these automation scripts were written in a old version of Puppet. \
+        So my job was to migrate these scripts in a much recent version of Puppet including good practices etc.',
       logo: 'https://bulma.io/images/placeholders/128x128.png',
       tags: ['Puppet', 'Ruby', 'Bash']
     }
