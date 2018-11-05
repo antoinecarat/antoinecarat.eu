815518b6c558382ca85211c1b648da9b62659874
cara-puce <antoine.carat@gmail.com>
2018-11-05 07:18:19 +0000

    Fix components structure - Timeline

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,100 +1,30 @@
 <template>
-    <div class="container">
-        <div class="hero-body">
-            <div class="container has-text-centered">
-                <img class="is-hidden-tablet" src="../../static/me.jpg" alt="My face :)" style="width: 75%"/>         
-                <img class="is-hidden-mobile" src="../../static/me.jpg" alt="My face :)" style="width: 15%"/>
-                <p class="subtitle"> Hi, nice to meet you! I'm Antoine Carat, a Web developer living in Nantes, France and relocating to Dublin, Ireland in September 2018. </p>
-                <p> Through this website you'll be able to check out my skillset, my previous expriences and my school and personnal projects as well. If you want to reach out, I would be glad to engage a conversation with you! <br> <br>
-                On the timeline just below, you can see how I came to here.</p>
-            </div>
-        </div>
-        <div class="steps">
-            <div class="step-item is-completed  ">
-                <div class="step-marker">
-                <span class="icon">
-                    <font-awesome-icon icon='parachute-box' />
-                </span>
-                </div>
-                <div class="step-details">
-                <p class="step-title">August 30th, 2018</p>
-                <p>Hello World!</p>
-                </div>
-            </div>
-            <div class="step-item is-completed">
-                <div class="step-marker">
-                    <span class="icon">
-                        <font-awesome-icon icon='heart' />
-                    </span>
-                </div>
-                <div class="step-details">
-                <p class="step-title">2011-2012</p>
-                <p>Fell in love with IT.</p>
-                </div>
-            </div>
-            <div class="step-item is-completed">
-                <div class="step-marker">
-                    <span class="icon">
-                        <font-awesome-icon icon='train' />
-                    </span>
-                </div>
-                <div class="step-details">
-                <p class="step-title">August, 2018</p>
-                <p>Moved to Nantes, France to study IT.</p>
-                </div>
-            </div>
-            <div class="step-item is-completed">
-                <div class="step-marker">
-                    <span class="icon">
-                        <font-awesome-icon icon='graduation-cap' />
-                    </span>
-                </div>
-                <div class="step-details">
-                <p class="step-title">July 16th, 2018</p>
-                <p>Got my Master's degree, ended at 1st rank.</p>
-                </div>
-            </div>
-            <div class="step-item">
-                <div class="step-marker">
-                    <span class="icon">
-                        <font-awesome-icon icon='plane-departure' />
-                    </span>
-                </div>
-                <div class="step-details">
-                    <p class="step-title">September, 2018</p>
-                    <p>Moving to Dublin, Ireland</p>
-                </div>
-            </div>
-        </div>
-    </div>    
+    <div class="container is-centered">
+        <h2 class="subtitle">Hi, nice to meet you!</h2>
+        <p class="content">
+            My name is Antoine, I'm a French Software Developper living in Dublin, Ireland. <br>
+            I've built up this website as  as a showcase, to let you come in my universe and see what I can do. <br>
+        </p>
+        <timeline title="Main events in my Software Developper life so far" :events=timelineEvents.events />
+    </div>
 </template>
 
 <script>
+import Timeline from "./views/Timeline.vue";
+import timelineEvents from './../data/en/timeline.js'
 
 export default {
   data: function () {
     return {
-      keywords: ['Antoine Carat', 'looking for a Wed Devloper role in Dublin, Ireland', 'a Graduated developer', 'a DevOps believer', 'a Cloud lover', 'an Open-source enthusiatic', 'a Human being'],
-      keywordID: 0,
+      timelineEvents
     }
   },
-  computed: {
-    keyword () { return this.keywords[this.keywordID] },
-  },
-  mounted: function () {
-    window.setInterval(() => {
-      this.keywordID = Math.floor(Math.random() * this.keywords.length)
-    }, 3000);
+  components : {
+      timeline: Timeline
   }
 }
 </script>
 
-<style scoped>
-    img {
-        margin-bottom: 3%;
-        border-width: 2%;
-        border-style: solid;
-        border-color: white;
-        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-    }
+<style>
+    
 </style>
---
+++ src/components/views/Timeline.vue
@@ -0,0 +1,165 @@
+<template>
+  <div class="timeline">
+    <span class="timeline-control" :class="{'is-disabled': !canMoveLeft()}" @click="moveLeft()" v-show="this.events != []">
+      <font-awesome-icon icon="caret-left" />
+    </span>
+    <div class="timeline-content">
+      <h3 class="timeline-title">{{title}}</h3>
+      <div class="events-wrapper">
+        <div class="timeline-placeholder" v-show="this.events.length < 1">
+          No event to show, sorry about that!
+        </div>
+        <div class="timeline-event" :class="{completed: event.completed}" v-for="event in displayedEvents" :key="event.title">
+          <div class="timeline-event-badge">
+              <font-awesome-icon :icon=event.icon />
+          </div>
+          <span class="timeline-event-dates">{{event.date}}</span>
+          <div class="timeline-event-content">
+            <p class="timeline-event-title">{{event.title}}</p>
+            <p class="timeline-event-text">{{event.text}}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <span class="timeline-control" :class="{'is-disabled': !canMoveRight()}" @click="moveRight()" v-show="this.events != []">
+      <font-awesome-icon icon="caret-right" />
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ""
+    },
+    events: {
+      type: Array,
+      default: []
+    }
+  },
+  data: function () {
+    return {
+      firstIndex: 0,
+      lastIndex: 6
+    }
+  },
+  computed: {
+    displayedEvents() {
+      return this.events.slice(this.firstIndex, this.lastIndex);
+    }
+  },
+  methods: {
+    canMoveLeft() {
+      return this.firstIndex != 0;
+    },
+    canMoveRight() {
+      return this.lastIndex != this.events.length;
+    },
+    moveLeft(){
+      if (this.firstIndex != 0) {
+        this.firstIndex -= 1;
+        this.lastIndex -= 1;
+      }
+    },
+    moveRight(){
+      if (this.lastIndex != this.events.length) {
+        this.firstIndex += 1;
+        this.lastIndex += 1;
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+.timeline {
+  display: flex;
+  flex-flow: row;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+    
+  .timeline-title {
+    margin: 0;
+  }
+
+  .timeline-control {
+    width: 2rem;
+    height: 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+
+    &.is-disabled {
+      color: gray;
+      cursor: default;
+    }
+  }
+
+  .timeline-content {
+    width: 90%;
+    overflow:  hidden;
+    position: relative;
+
+    .events-wrapper {
+      display: flex;
+      flex-flow: row nowrap;
+      justify-content: center;
+      align-items: flex-start;
+    }
+
+    .timeline-event {
+      min-width: 15%;
+      max-width: 15%;
+      padding: 1% 0;
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      align-items: center;
+
+      .timeline-event-badge {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 2.5rem;
+        height: 2.5rem;
+        border: .2rem solid rgba(128, 128, 128, 0.5);
+        border-radius: 50%;
+        position: relative;
+        margin-bottom: 5%;
+      }
+
+      &.completed .timeline-event-badge {
+        border-color: rgba(30, 144, 255, 0.7);
+      }
+
+      .timeline-event-dates {
+        font-weight: 600;
+      }
+
+      .timeline-event-content {
+        display:  flex;
+        flex-flow: column wrap;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        margin: 0 5%;
+
+        .timeline-event-title {
+          font-size: 1rem;
+          margin: 5% 0 10% 0;
+        }
+
+        .timeline-event-text {
+          font-size: .8rem;
+        }
+      }
+    }
+  }
+}
+
+</style>
