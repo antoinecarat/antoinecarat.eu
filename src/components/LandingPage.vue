7bd417efb369e3fe70cbb168f64b666f5b734354
cara-puce <antoine.carat@gmail.com>
2018-11-29 00:11:11 +0000

    Fix landing page style

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,24 +1,24 @@
 <template>
-    <div class="container is-centered">
-        <div class="intro container is-row is-centered">
-            <img class="image" src="../../static/me.jpg" alt="">
-            <div class="container is-two-thirds">
-                <h2>Hey, nice to meet you!</h2>
-                <p>
-                My name is Antoine, I'm a French Software Engineer graduated from <em>Université des Sciences et Technique de Nantes.</em>
-                </p>
-                <p>
-                I'm currently living in Dublin, Ireland where I'm working at BrowserStack Ltd.
-                </p>
-                <p>
-                I've built up this website as  as a showcase, to let you come in my universe and see what I can do.
-                <br>
-                Please, feel free to reach out if you have any question, comment or if you just want to talk!
-                </p>
-            </div>
-        </div>
-        <timeline title="What happened in my Software Engineer life so far:" :events=timelineEvents.events />
+  <div>
+    <div class="intro">
+      <img class="image" src="../../static/me.jpg" alt="">
+      <div class="text">
+        <h2>Hey, nice to meet you!</h2>
+        <p>
+        My name is Antoine, I'm a French Software Engineer graduated from <em>Université des Sciences et Technique de Nantes.</em>
+        </p>
+        <p>
+        I'm currently living in Dublin, Ireland where I'm working at BrowserStack Ltd.
+        </p>
+        <p>
+        I've built up this website as  as a showcase, to let you come in my universe and see what I can do.
+        <br>
+        Please, feel free to reach out if you have any question, comment or if you just want to talk!
+        </p>
+      </div>
     </div>
+    <timeline title="What happened in my Software Engineer life so far" :events=timelineEvents.events />
+  </div>
 </template>
 
 <script>
@@ -32,18 +32,29 @@ export default {
     }
   },
   components : {
-      timeline: Timeline
+    timeline: Timeline
   }
 }
 </script>
 
 <style lang="scss">
-    .image {
-        width: 20%;
-        box-shadow: 0 0 3rem 0.2rem black;
+.intro {
+  display: flex;
+  flex-flow: column wrap;
+  justify-content: center;
+  align-items: center;
+  margin: 3%;
+
+  .image {
+    @media only screen and (max-width: 768px) {
+      width: 60%;
     }
-    
-    .intro {
-        margin: 3%;
-    }
+    width: 20%;
+    box-shadow: 0 0 3rem 0.2rem black;
+  }
+
+  .text {
+    text-align: center;
+  }
+}
 </style>
