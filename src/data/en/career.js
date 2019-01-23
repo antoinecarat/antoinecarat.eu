a442d5dcfc0793f7bb3c724298a2fcfb3777ac97
Antoine Carat <antoine.carat@gmail.com>
2019-01-23 00:48:12 +0000

    Clean up post-design refact

--- src/data/en/career.js
+++ src/data/en/career.js
@@ -1,27 +1,28 @@
 export default {
   experiences: [
     {
-      title: 'DevOps Intern',
-      location: 'VSC-Technologies, Nantes, France',
-      dates: '18/04/2018 - 13/07/2018',
-      description:
-        'Chaos tests automation',
+      title: 'Software Engineer',
+      location: 'Browserstack Ltd, Dublin, Ireland',
+      start: '05/11/2018',
+      description: 'Software Engineer for Mobile Platform.',
       logo: 'https://bulma.io/images/placeholders/128x128.png',
-      tags: ['Chaos engineering', 'Python', 'Jenkins', 'AWS']
+      tags: ['Ruby', 'Node.js', 'Bash', 'Mobile', 'Android', 'iOS']
     },
     {
-      title: 'Cloud Intern',
+      title: 'Cloud & DevOps Intern',
       location: 'VSC-Technologies, Nantes, France',
-      dates: '15/01/2018 - 18/04/2018',
+      start: '15/01/2018',
+      end: '13/07/2018',
       description:
-        'OpenStack prod platform management and customer support',
+        'Chaos tests automation + OpenStack prod platform management and customer support',
       logo: 'https://bulma.io/images/placeholders/128x128.png',
-      tags: ['OpenStack', 'Docker', 'Docker-Swarm', 'Jenkins', 'OpenFaas', 'Angular', 'Go']
+      tags: ['OpenStack', 'Docker', 'Docker-Swarm', 'Jenkins', 'OpenFaas', 'Angular', 'Go', 'Chaos engineering', 'Python', 'AWS']
     },
     {
       title: 'Cloud Research Intern',
       location: 'Discovery initiative, IMT, Nantes, France',
-      dates: '15/06/2017 - 15/08/2017',
+      start: '15/06/2017',
+      end: '15/08/2017',
       description:
         'During this volunteer internship I continue to work on the highly-distributed OpenStack project led by Discovery initiative. \
          This time, I was in charge of moving one OpenStack service backend from a relationnal database (mySQL) to a distributed one (CockroachDB).',
@@ -31,7 +32,8 @@ export default {
     {
       title: 'Research initiation Internship',
       location: 'Discovery initiative, IMT, Nantes, France',
-      dates: '15/01/2017 - 15/04/2017',
+      start: '15/01/2017',
+      end:'15/04/2017',
       description:
         'Once a week, we worked with the Discovery initiative team around their highly-distributed OpenStack project in order to discover the research world. \
          Our work was to add a code profiling tool to Enos, a tool developped by the team that allows to launch experiments on OpenStack.',
@@ -39,10 +41,10 @@ export default {
       tags: ['OpenStack', 'Ansible', 'Python', 'Research']
     },
     {
-      title:
-        'Non-profit student organisation founding president: Alice&Bob',
-      location: 'Sciences University, Nantes, France',
-      dates: '25/10/2016 - 30/10/2017',
+      title: 'Non-profit student organisation founding president',
+      location: 'Alice&Bob, Sciences University, Nantes, France',
+      start: '25/10/2016',
+      end: '30/10/2017',
       description:
         'I founded and take care of a non-profit organisation in my University. The goal of Alice&Bob is to build up a "school-spirit" between all nantes\' \
          University IT students by helping them in their shoolarship by organizing events like professionnal meetings, school program explainations, ... \
@@ -53,7 +55,8 @@ export default {
     {
       title: 'Constraint Programming Intern',
       location: 'TASC team, EMN, Nantes, France',
-      dates: '18/04/2016 - 08/07/2016',
+      start: '18/04/2016',
+      end: '08/07/2016',
       description:
         'I ended my Bachelor degree as a two-months intern in the research team TASC, standing for Theorie, Algorithms and Constraints Systems. \
         During this internship, I was in charge of enhancing a Constraint Problem solver named Choco by externalizing all linear constraints to a dedicated solver: IntSat.',
@@ -63,7 +66,8 @@ export default {
     {
       title: 'Automation and Deployment Intern',
       location: 'La Poste DTC, Nantes, France',
-      dates: '18/04/2014 - 30/06/2014',
+      start: '18/04/2014',
+      end: '30/06/2014',
       description:
         'The last two month of DUT are dedicated to an internship. I made mine in the Technical Direction of the french postal service, La Poste. \
         Mail-sorting engines and servers deployements are automated with Puppet, a automation language written in Ruby. \
--- src/main.js
+++ src/main.js
@@ -3,14 +3,44 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import store from './store'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faSeedling, faShoePrints, faVial, faComment, faComments, faParachuteBox, faTrain, faGraduationCap, faPlaneDeparture, faHeart, faSearch, faHandshake, faCaretLeft, faCaretRight, faHome, faTag } from '@fortawesome/free-solid-svg-icons'
+import {
+  faShoePrints,
+  faVial,
+  faComment,
+  faComments,
+  faParachuteBox,
+  faTrain,
+  faGraduationCap,
+  faPlaneDeparture,
+  faHeart,
+  faSearch,
+  faHandshake,
+  faCaretLeft,
+  faCaretRight,
+  faHome,
+  faTag
+} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-library.add(faSeedling, faShoePrints, faVial, faComment, faComments, faParachuteBox, faTrain, faGraduationCap, faPlaneDeparture, faHeart, faSearch, faHandshake, faCaretLeft, faCaretRight, faHome, faTag)
+library.add(
+  faShoePrints,
+  faVial,
+  faComment,
+  faComments,
+  faParachuteBox,
+  faTrain,
+  faGraduationCap,
+  faPlaneDeparture,
+  faHeart,
+  faSearch,
+  faHandshake,
+  faCaretLeft,
+  faCaretRight,
+  faHome,
+  faTag
+)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
-
 
 require('./assets/sass/main.scss')
 
@@ -19,7 +49,6 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  store,
   components: { App },
   template: '<App/>'
 })
