3c35dee93c398ca7ec2d35ee4730eee29a423ca7
Antoine Carat <antoine.carat@gmail.com>
2019-03-29 23:11:14 +0000

    Extract header component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,20 @@
 <template>
   <section class="box">
-    <div class="header" :class="{'active': loaded}">
-      <h1 class="big-title" @click="$router.push('/')">ANTOINE CARAT</h1>
-      <div class="contacts">
-        <span>Mail</span> — <span>LinkedIn</span> — <span>Github</span> — <span>Twitter</span> — <span>Medium</span> — <span>StackOverflow</span>
-      </div>
-    </div>
+    <Header :loaded=this.loaded />
     <router-view></router-view>
   </section>
 </template>
 
 <script>
+import Header from './components/Header/Header.vue'
 
 export default {
   name: 'App',
+  components: {
+    Header
+  },
   data () {
-    return { 
+    return {
       loaded: false
     }
   },
@@ -32,45 +31,5 @@ export default {
     justify-content: center;
     align-items: center;
     width: 100%;
-
-    .header {
-      width: 75%;
-      text-align: center;
-      position: relative;
-      top: -25%;
-      opacity: 0;
-      transition: 300ms;
-
-      &.active {
-        top: 0;
-        opacity: 100;
-      }
-
-      .big-title {
-        color: #008cb4;
-        font-family: 'Tahoma', 'Myriad', 'Adora', 'Avenir', 'sans-serif';
-        font-size: 500%;
-        font-weight: 100;
-        letter-spacing: .2rem;
-        margin: 1rem 0 .5rem 0;
-        cursor: pointer;
-        &:hover {
-          color: darken($color: #008cb4, $amount: 5)
-        }
-      }
-
-      .contacts {
-        width: 100%;
-        padding: .5rem 0;
-        color: #008cb4;
-        border-top: 1px solid gray;
-        border-bottom: 1px solid gray;
-
-        display: flex;
-        flex-flow: row wrap;
-        justify-content: space-evenly;
-        align-items: center;
-      }
-    }
   }
 </style>
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="header" :class="{'active': loaded}">
+    <h1 class="big-title" @click="$router.push('/')">ANTOINE CARAT</h1>
+    <div class="contacts">
+      <span class="contact" v-for="link in links" :key="link.name">
+        <a :href="link.link">{{ link.name }}</a>
+      </span>
+      <!-- <span>Mail</span> — <span>LinkedIn</span> — <span>Github</span> — <span>Twitter</span> — <span>Medium</span> — <span>StackOverflow</span> -->
+    </div>
+  </div>
+</template>
+
+<script>
+import headerData from './header.json'
+
+export default {
+  data: function(){
+    return { ...headerData }
+  },
+  props: {
+    loaded: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+
+<style lang="scss">
+  .header {
+    width: 75%;
+    text-align: center;
+    position: relative;
+    top: -25%;
+    opacity: 0;
+    transition: 300ms;
+
+    &.active {
+      top: 0;
+      opacity: 100;
+    }
+
+    .big-title {
+      color: #008cb4;
+      font-family: 'Tahoma', 'Myriad', 'Adora', 'Avenir', 'sans-serif';
+      font-size: 500%;
+      font-weight: 100;
+      letter-spacing: .2rem;
+      margin: 1rem 0 .5rem 0;
+      cursor: pointer;
+      &:hover {
+        color: darken($color: #008cb4, $amount: 10)
+      }
+    }
+
+    .contacts {
+      width: 100%;
+      padding: .5rem 0;
+      color: #008cb4;
+      border-top: 1px solid gray;
+      border-bottom: 1px solid gray;
+
+      display: flex;
+      flex-flow: row wrap;
+      justify-content: space-evenly;
+      align-items: center;
+    }
+  }
+</style>
