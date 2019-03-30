c5a9f2debcd18f683e6a081456b09b7386e6a479
Antoine Carat <antoine.carat@gmail.com>
2019-03-30 13:58:33 +0000

    :arrow_up: Upgrade vue-cli

--- src/main.js
+++ src/main.js
@@ -1,54 +1,12 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
-import { library } from '@fortawesome/fontawesome-svg-core'
-import {
-  faShoePrints,
-  faVial,
-  faComment,
-  faComments,
-  faParachuteBox,
-  faTrain,
-  faGraduationCap,
-  faPlaneDeparture,
-  faHeart,
-  faSearch,
-  faHandshake,
-  faCaretLeft,
-  faCaretRight,
-  faHome,
-  faTag
-} from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-library.add(
-  faShoePrints,
-  faVial,
-  faComment,
-  faComments,
-  faParachuteBox,
-  faTrain,
-  faGraduationCap,
-  faPlaneDeparture,
-  faHeart,
-  faSearch,
-  faHandshake,
-  faCaretLeft,
-  faCaretRight,
-  faHome,
-  faTag
-)
-Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 require('./assets/sass/main.scss')
 
 Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(App)
+}).$mount('#app')
