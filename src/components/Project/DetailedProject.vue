6d715f129e0e7a414c21ef3db6d79395618ebda7
Antoine Carat <antoine.carat@gmail.com>
2019-04-23 12:53:13 +0100

    Enhance projects page

--- src/components/Project/DetailedProject.vue
+++ src/components/Project/DetailedProject.vue
@@ -1,3 +1,63 @@
 <template>
-  <p>PROJECT N {{ $route.params.id }}</p>
+  <div class="experience-detailed">
+    <p class="back" @click="$router.push('/projects')">Back</p>
+    <SlidingDoors>
+      <div class="card" slot="left">
+        <!-- Show thumbnail + title + stats -->
+        <img class="logo" :src="project.thumbnail || 'https://bulma.io/images/placeholders/480x320.png'" alt="" />
+        <h2 class="p-title">{{ project.title }}</h2>
+      </div>
+      <div class="description" slot="right">
+        <h2>Project description</h2>
+        <p>{{ project.description.short }}</p>
+        <h2>How?</h2>
+        <p>Technical stuff</p>
+      </div>
+    </SlidingDoors>
+  </div>
 </template>
+
+<script>
+import projectData from "./projects.json";
+
+export default {
+  data: function() {
+    return {
+      project: projectData.projects[this.$route.params.id]
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+.project-detailed {
+  width: 75%;
+  margin-top: 1rem;
+  text-align: center;
+  position: relative;
+
+  .back {
+    position: absolute;
+    text-align: left;
+    color: #008cb4;
+    cursor: pointer;
+    text-decoration: underline;
+    &:active,
+    &:hover {
+      font-weight: 600;
+    }
+    &::before {
+      content: "< ";
+    }
+  }
+
+  .card {
+    .logo {
+      // width: 80%;
+    }
+  }
+  .description {
+    text-align: left;
+  }
+}
+</style>
--- src/components/Project/Projects.vue
+++ src/components/Project/Projects.vue
@@ -4,29 +4,22 @@
     <SlidingDoors>
       <div class="card" slot="left">
         <h2>Github Activity</h2>
-          <GithubCalendar user="antoinecarat" />
-          Commits, Pull Requests, Issues and Reviews
+        <GithubCalendar user="antoinecarat" />
+        Commits, Pull Requests, Issues and Reviews
       </div>
       <div class="description" slot="right">
-        <!-- Grid -->
-        <h2>Highlights</h2>
+        <h2>Projects</h2>
         <div class="projects-grid">
           <div
             class="project-card"
-            v-for="project in projects.filter(e => e.highlight)"
-            :key="project.title"
+            v-for="(project, i) in projects"
+            :key="project.title" @click="$router.push(`/project/${i}`)"
           >
-            {{ project.title }}
-          </div>
-        </div>
-        <h2>More</h2>
-        <div class="projects-grid">
-          <div
-            class="project-card"
-            v-for="project in projects.filter(e => !e.highlight)"
-            :key="project.title"
-          >
-            {{ project.title }}
+            <img class="thumbnail" :src="project.thumbnail || 'https://bulma.io/images/placeholders/480x320.png'" alt="Project's thumbnail">
+            <div class="project-data">
+              <h2>{{ project.title }}</h2>
+              <h3>{{ project.subtitle }}</h3>
+            </div>
           </div>
         </div>
       </div>
@@ -75,6 +68,7 @@ export default {
   }
 
   .card {
+
     .logo {
       border-radius: 100rem;
       width: 30%;
@@ -89,15 +83,60 @@ export default {
     text-align: left;
     .projects-grid {
       display: flex;
-      flex-flow: row wrap;
-      justify-content: space-evenly;
-      align-content: space-between;
-      align-items: flex-start;
+      flex-flow: column nowrap;
+      justify-content: flex-start;
+      align-items: center;
+
+      @media only screen and (min-width: 768px) {
+        // border: 1px solid lightgray;
+        height: 22rem;
+        overflow: auto;
+        box-shadow: inset 0px -.5rem .5rem -.5rem #000000;
+      }
+      
 
       .project-card {
-        width: 40%;
-        border: 1px solid black;
+        width: 95%;
+        border: 1px solid lightgray;
+        border-radius: 1rem;
+        padding: .2rem;
+        transition: 600ms;
         margin-bottom: 1rem;
+        cursor: pointer;
+        z-index: -1;
+
+        display: flex;
+        flex-flow: row wrap;
+        justify-content: flex-start;
+        align-items: center;
+
+        &:hover {
+          @media only screen and (min-width: 768px) {
+            width: calc(100% - .5rem);
+          }
+          border: 1px solid #008cb4;
+        }
+        
+        h2 {
+          align-self: flex-start;
+          margin: 1rem 0 0 0;
+        }
+
+        h3 {
+          align-self: flex-start;
+          margin: .5rem 0;
+        }
+
+        .thumbnail {
+          width: 5rem;
+          height: 5rem;
+          border-radius: 1rem;
+          margin-right: 1rem;
+          object-fit: cover;
+        }
+
+
+
       }
     }
   }
